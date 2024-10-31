1b7716b6b9252b9d7e222ecea2e0ac7645c48b74
Jordan Campbell <jordancampbell.work@gmail.com>
2024-10-31 17:14:33 -0500

    Exercise 3 - Look up any Superhero by typing in the name or aliasin a Textfield

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const loadSuperHeroes = () => {
-  fetch("superheroes.php")
-    .then((response) => response.text())
-    .then((data) => {
-      alert(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
 window.onload = () => {
   searchBtn = document.getElementById("search-btn");
 
-  searchBtn.addEventListener("click", loadSuperHeroes);
+  searchBtn.addEventListener("click", () => {
+    const textbox = document.getElementById("superhero-input");
+    const container = document.getElementById("result");
+
+    let userInput = textbox.value;
+    userInput.trim();
+
+    if (userInput === "") {
+      fetch("superheroes.php")
+        .then((response) => response.text())
+        .then((data) => {
+          container.innerHTML = data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      fetch(`superheroes.php?query=${encodeURIComponent(userInput)}`)
+        .then((response) => response.text())
+        .then((data) => {
+          container.innerHTML = data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  });
 };
